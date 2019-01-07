49927a96ea89645bef6c2af0a7e9f6375f0e9e5a
franzecs@gmail.com <franzecs@gmail.com>
2019-01-07 08:33:08 -0300

    correção cors

--- src/main/java/com/ikytus/akademy/resources/AuthResource.java
+++ src/main/java/com/ikytus/akademy/resources/AuthResource.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import com.ikytus.akademy.security.UserService;
 
 @RestController
 @RequestMapping(value = "/auth")
+@CrossOrigin(origins = "*")
 public class AuthResource {
 
 	@Autowired
