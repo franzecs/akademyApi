f848a883434caccbfca0ac5749345d35ed42d879
Francisco José C. Silva <franzecs@gmail.com>
2019-08-15 07:35:50 -0300

    New fluxo de caixa

---
+++ src/main/java/com/ikytus/akademy/domain/FCx.java
@@ -0,0 +1,128 @@
+package com.ikytus.akademy.domain;
+
+import java.io.Serializable;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.ikytus.akademy.domain.enums.TipoFCx;
+
+@Document
+public class FCx implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private String id;
+	private int mes;
+	private int ano;
+	private Integer tipo;
+	private String dia;
+	private String descricao;
+	private Double valor;
+	private String status;
+
+	public FCx() {
+	}
+
+	public FCx(String id, int mes, int ano, TipoFCx tipo, String dia, String descricao, Double valor,
+			String status) {
+		super();
+		this.id = id;
+		this.mes = mes;
+		this.ano = ano;
+		this.tipo = (tipo == null) ? null : tipo.getCod();
+		this.dia = dia;
+		this.descricao = descricao;
+		this.valor = valor;
+		this.status = status;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public int getMes() {
+		return mes;
+	}
+
+	public void setMes(int mes) {
+		this.mes = mes;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+
+	public TipoFCx getTipo() {
+		return TipoFCx.toEnum(tipo);
+	}
+
+	public void setTipo(TipoFCx tipo) {
+		this.tipo = tipo.getCod();
+	}
+
+	public String getDia() {
+		return dia;
+	}
+
+	public void setDia(String dia) {
+		this.dia = dia;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		FCx other = (FCx) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
